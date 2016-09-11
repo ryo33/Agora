893e741bad018a82191ad8fbdefe5a0173b5686b
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-09-12 00:39:28 +0900

    Fix the bug resources are not fetched sometimes

--- web/static/js/sagas/resources.js
+++ web/static/js/sagas/resources.js
@@ -1,5 +1,5 @@
-import { fork, take, put, select, call, race } from 'redux-saga/effects';
-import { takeEvery, channel } from 'redux-saga';
+import { fork, take, put, select, call, race, actionChannel } from 'redux-saga/effects';
+import { takeEvery, channel, delay } from 'redux-saga';
 
 import { commonChannel, pushMessage } from 'socket';
 
@@ -8,7 +8,10 @@ import {
     prepareGroups, prepareThreads, preparePosts, prepareUsers
 } from 'actions/resources';
 
-function* fetchResources(resource, resultChannel, ids) {
+const TIMEOUT = 1500;
+
+function* fetchResources(resource, resultChannel, idsSet) {
+  const ids = Array.from(idsSet);
   const havingResources = yield select(({ [resource]: resources }) => resources);
   const lackingIDs = ids.filter(id => ! havingResources.hasOwnProperty(id));
   if (lackingIDs.length != 0) {
@@ -20,53 +23,71 @@ function* fetchResources(resource, resultChannel, ids) {
   }
 }
 
-function* watchFetchingAction(actionChannel, action) {
-  yield call(actionChannel.put, action);
-}
-
 function createWatcherFor(resource, prepare, add) {
   return function*() {
     let fetchingIDs = new Set();
     let waitingIDs = new Set();
-    let fetching = false;
+    let fetchingTask = false;
+    let countDown = TIMEOUT;
+    let lastTime = Date.now();
     const resultChannel = channel();
-    const actionChannel = channel();
+    const resourceChannel = yield actionChannel(prepare.getType());
 
-    function* fetch(resource, resultChannel, ids) {
-      fetching = true;
-      ids.forEach(id => {
-        fetchingIDs.add(id);
-      });
-      yield fork(fetchResources, resource, resultChannel, ids);
+    function swapIDs() {
+      const tmp = fetchingIDs;
+      fetchingIDs = waitingIDs;
+      waitingIDs = tmp;
+      waitingIDs.clear();
     }
 
-    yield fork(takeEvery, prepare.getType(), watchFetchingAction, actionChannel);
+    function* fetch() {
+      if (fetchingIDs.size != 0) {
+        countDown = TIMEOUT;
+        fetchingTask = yield fork(fetchResources, resource, resultChannel, fetchingIDs);
+      }
+    }
 
     while (true) {
-      const { result, fetchingAction } = yield race({
-        result: take(resultChannel),
-        fetchingAction: take(actionChannel)
-      });
-      if (result) {
-        fetching = false;
-        fetchingIDs.clear();
-        if (waitingIDs.size != 0) {
-          yield* fetch(resource, resultChannel, Array.from(waitingIDs.values()));
-          fetchingIDs = waitingIDs;
-          waitingIDs = new Set();
+      const now = Date.now();
+      countDown -= (now - lastTime);
+      lastTime = now;
+      let res;
+      if (fetchingTask) {
+        res = yield race({
+          result: take(resultChannel),
+          fetchingAction: take(resourceChannel),
+          timeout: call(delay, countDown)
+        });
+      } else {
+        const action = yield take(resourceChannel);
+        res = { fetchingAction: action };
+      }
+      const { result, fetchingAction, timeout } = res;
+
+      if (timeout) {
+        if (fetchingTask) {
+          for(const id of waitingIDs.values()) {
+            fetchingIDs.add(id);
+          }
+          yield* fetch();
         }
+      } else if (result) {
+        fetchingTask = null;
+        swapIDs();
+        yield* fetch();
         yield put(add(result));
       } else {
         const ids = fetchingAction.payload;
         if (ids.length != 0) {
-          if (fetching) {
+          if (fetchingTask) {
             ids.forEach(id => {
-              if (!fetchingIDs.has(id)) {
-                waitingIDs.add(id);
-              }
+              if (!fetchingIDs.has(id)) { waitingIDs.add(id); }
             });
           } else {
-            yield* fetch(resource, resultChannel, ids);
+            ids.forEach(id => {
+              fetchingIDs.add(id);
+            });
+            yield* fetch();
           }
         }
       }
