f56b5d4569481234fe51b48a224399b7813fadc8
Shoya Matsumoto <m42m0s@gmail.com>
2016-11-03 10:02:09 +0900

    [WIP]Enhance ui (#61)
    * Change font size
    
    * Change Post
    
    * Fix

--- web/static/js/components/Post.js
+++ web/static/js/components/Post.js
@@ -8,6 +8,7 @@ import Paper from 'material-ui/Paper'
 import FlatButton from 'material-ui/FlatButton'
 import Linkify from 'react-linkify'
 import ReactTooltip from 'react-tooltip'
+import { grey400, grey200 } from 'material-ui/styles/colors'
 
 import { MembershipIcon } from 'components/icons'
 import Unimplemented from 'components/Unimplemented'
@@ -21,6 +22,7 @@ import { requirePost, checkPostOwned } from 'hocs/resources'
 const actionCreators = {
   push,
 }
+
 
 class Post extends Component {
   constructor(props) {
@@ -39,7 +41,14 @@ class Post extends Component {
     const tooltipID = 'post_membership_' + id
     return (
       <span>
-        <span><MembershipIcon data-tip data-for={tooltipID} /></span>
+        <span>
+          <MembershipIcon
+            data-tip data-for={tooltipID}
+            style={{
+              fontSize: '1em'
+            }}
+          />
+        </span>
         <ReactTooltip id={tooltipID} type='dark' effect='solid'>Member</ReactTooltip>
       </span>
     )
@@ -49,17 +58,13 @@ class Post extends Component {
     const { isOwned, id, post, push, members } = this.props
     const { open } = this.state
     const isMember = members ? members.includes(post.user_id) : false
+    const title_color = isOwned ? grey400 : grey200
     return (
-      <div>
-        <ListItem
-          handleClick={this.click}
-          isOwned={isOwned}
-        >
-          {
-            post.post_id
-            ? <div><PostButton id={post.post_id} /></div>
-            : null
-          }
+      <Paper
+        onClick={this.click}
+        style={{margin: "0.5%", borderRadius: '2.5px'}}
+      >
+        <div style={{backgroundColor: title_color, padding: '0.1em 0.5em'}}>
           <ResourceTitle
             user={post.user_id}
             title={post.title}
@@ -71,25 +76,30 @@ class Post extends Component {
               ? this.renderMembership(id)
               : null
           }
-          <pre
-            style={{
-              fontSize: '1.0em',
-              whiteSpace: 'pre-wrap',
-              margin: '0px',
-              padding: '8px 8px',
-            }}
-          >
-            <Linkify properties={{ target: '_blank' }}>
-              {post.text}
-            </Linkify>
-          </pre>
-        </ListItem>
+        </div>
+        {
+          post.post_id
+            ? <div><PostButton id={post.post_id} /></div>
+            : null
+        }
+        <pre
+          style={{
+            fontSize: '1.0em',
+            whiteSpace: 'pre-wrap',
+            margin: '0px',
+            padding: '8px 8px',
+          }}
+        >
+          <Linkify properties={{ target: '_blank' }}>
+            {post.text}
+          </Linkify>
+        </pre>
         {
           open
           ? <PostActions id={id} />
           : null
         }
-      </div>
+      </Paper>
     )
   }
 }
